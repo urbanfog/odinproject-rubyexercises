a3d3fae8c3ef47fcf560cf0fe923b66b8c381d69
James Smith <theworld969@hotmail.com>
2020-05-12 17:12:50 -0600

    Finish exercise for now

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -19,42 +19,40 @@ class Tree
   def build_tree
     # transform array into sorted set with no duplications
     sorted_array = @array.uniq.sort # => [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345]
-    @root = Node.new(sorted_array[sorted_array.length / 2]) # => #<Node:0x00007febc68e3140 @value=8, @left=nil, @right=nil>
+    @root = Node.new(sorted_array[sorted_array.length / 2]) # => #<Node:0x00007faace89e178 @value=8, @left=nil, @right=nil>
     sorted_array.delete(@root.value) # => 8
-    
     sorted_array.each do |e|
-      current_node = @root # pointer
-      new_node = Node.new(e) # => #<Node:0x00007febc68e1e80 @value=1, @left=nil, @right=nil>, #<Node:0x00007febc68eff58 @value=3, @left=nil, @right=nil>, #<Node:0x00007febc68ed1e0 @value=4, @left=nil, @right=nil>, #<Node:0x00007febc6888ee8 @value=5, @left=nil, @right=nil>, #<Node:0x00007febc68681e8 @value=7, @left=nil, @right=nil>, #<Node:0x00007febc683b6e8 @value=9, @left=nil, @right=nil>, #<Node:0x00007febc68314b8 @value=23, @left=nil, @right=nil>, #<Node:0x00007febc681a3f8 @value=67, @left=nil, @right=nil>, #<Node:0x00007febc6812ea0 @value=324, @left=nil, @right=nil>, #<Node:0x00007febc6809238 @value=6345, @left=nil, @right=nil>
-      
-      while current_node != nil
-        case new_node.value <=> current_node.value # => -1, -1, 1, -1, 1, 1, -1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-        when -1 # less than
-          if current_node.left == nil
-            current_node.left = new_node
-            break
-          else
-            current_node = current_node.left # => #<Node:0x00007febc68e1e80 @value=1, @left=nil, @right=nil>, #<Node:0x00007febc68e1e80 @value=1, @left=nil, @right=#<Node:0x00007febc68eff58 @value=3, @left=nil, @right=nil>>, #<Node:0x00007febc68e1e80 @value=1, @left=nil, @right=#<Node:0x00007febc68eff58 @value=3, @left=nil, @right=#<Node:0x00007febc68ed1e0 @value=4, @left=nil, @right=nil>>>, #<Node:0x00007febc68e1e80 @value=1, @left=nil, @right=#<Node:0x00007febc68eff58 @value=3, @left=nil, @right=#<Node:0x00007febc68ed1e0 @value=4, @left=nil, @right=#<Node:0x00007febc6888ee8 @value=5, @left=nil, @right=nil>>>>
-          end
-        when 1 # greater than
-          if current_node.right == nil
-            current_node.right = new_node
-            break
-          else
-            current_node = current_node.right # => #<Node:0x00007febc68eff58 @value=3, @left=nil, @right=nil>, #<Node:0x00007febc68eff58 @value=3, @left=nil, @right=#<Node:0x00007febc68ed1e0 @value=4, @left=nil, @right=nil>>, #<Node:0x00007febc68ed1e0 @value=4, @left=nil, @right=nil>, #<Node:0x00007febc68eff58 @value=3, @left=nil, @right=#<Node:0x00007febc68ed1e0 @value=4, @left=nil, @right=#<Node:0x00007febc6888ee8 @value=5, @left=nil, @right=nil>>>, #<Node:0x00007febc68ed1e0 @value=4, @left=nil, @right=#<Node:0x00007febc6888ee8 @value=5, @left=nil, @right=nil>>, #<Node:0x00007febc6888ee8 @value=5, @left=nil, @right=nil>, #<Node:0x00007febc683b6e8 @value=9, @left=nil, @right=nil>, #<Node:0x00007febc683b6e8 @value=9, @left=nil, @right=#<Node:0x00007febc68314b8 @value=23, @left=nil, @right=nil>>, #<Node:0x00007febc68314b8 @value=23, @left=nil, @right=nil>, #<Node:0x00007febc683b6e8 @value=9, @left=nil, @right=#<Node:0x00007febc68314b8 @value=23, @left=nil, @right=#<Node:0x00007febc68...
-          end
-        when 0 # equal
-          return "Error, duplicate number detected."
-        when nil # when encounters nil, shouldn't happen??
-          current_node = nil 
+      insert(e)
+    end
+    @root
+  end
+
+  def insert(node)
+    current_node = @root # pointer
+    new_node = Node.new(node) # => #<Node:0x00007faace89dd40 @value=1, @left=nil, @right=nil>, #<Node:0x00007faace89d958 @value=3, @left=nil, @right=nil>, #<Node:0x00007faace89d228 @value=4, @left=nil, @right=nil>, #<Node:0x00007faace89c6e8 @value=5, @left=nil, @right=nil>, #<Node:0x00007faacf057158 @value=7, @left=nil, @right=nil>, #<Node:0x00007faace8a3970 @value=9, @left=nil, @right=nil>, #<Node:0x00007faace8a35d8 @value=23, @left=nil, @right=nil>, #<Node:0x00007faace8a2ed0 @value=67, @left=nil, @right=nil>, #<Node:0x00007faace8a23b8 @value=324, @left=nil, @right=nil>, #<Node:0x00007faace8a13f0 @value=6345, @left=nil, @right=nil>
+    
+    while current_node != nil
+      case new_node.value <=> current_node.value # => -1, -1, 1, -1, 1, 1, -1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+      when -1 # less than
+        if current_node.left == nil
+          current_node.left = new_node
+          break
+        else
+          current_node = current_node.left # => #<Node:0x00007faace89dd40 @value=1, @left=nil, @right=nil>, #<Node:0x00007faace89dd40 @value=1, @left=nil, @right=#<Node:0x00007faace89d958 @value=3, @left=nil, @right=nil>>, #<Node:0x00007faace89dd40 @value=1, @left=nil, @right=#<Node:0x00007faace89d958 @value=3, @left=nil, @right=#<Node:0x00007faace89d228 @value=4, @left=nil, @right=nil>>>, #<Node:0x00007faace89dd40 @value=1, @left=nil, @right=#<Node:0x00007faace89d958 @value=3, @left=nil, @right=#<Node:0x00007faace89d228 @value=4, @left=nil, @right=#<Node:0x00007faace89c6e8 @value=5, @left=nil, @right=nil>>>>
         end
+      when 1 # greater than
+        if current_node.right == nil
+          current_node.right = new_node
+          break
+        else
+          current_node = current_node.right # => #<Node:0x00007faace89d958 @value=3, @left=nil, @right=nil>, #<Node:0x00007faace89d958 @value=3, @left=nil, @right=#<Node:0x00007faace89d228 @value=4, @left=nil, @right=nil>>, #<Node:0x00007faace89d228 @value=4, @left=nil, @right=nil>, #<Node:0x00007faace89d958 @value=3, @left=nil, @right=#<Node:0x00007faace89d228 @value=4, @left=nil, @right=#<Node:0x00007faace89c6e8 @value=5, @left=nil, @right=nil>>>, #<Node:0x00007faace89d228 @value=4, @left=nil, @right=#<Node:0x00007faace89c6e8 @value=5, @left=nil, @right=nil>>, #<Node:0x00007faace89c6e8 @value=5, @left=nil, @right=nil>, #<Node:0x00007faace8a3970 @value=9, @left=nil, @right=nil>, #<Node:0x00007faace8a3970 @value=9, @left=nil, @right=#<Node:0x00007faace8a35d8 @value=23, @left=nil, @right=nil>>, #<Node:0x00007faace8a35d8 @value=23, @left=nil, @right=nil>, #<Node:0x00007faace8a3970 @value=9, @left=nil, @right=#<Node:0x00007faace8a35d8 @value=23, @left=nil, @right=#<Node:0x00007faace8a2...
+        end
+      when 0 # equal
+        return "Error, duplicate number detected."
+      when nil # when encounters nil, shouldn't happen??
+        current_node = nil 
       end
     end
-
-    @root # => #<Node:0x00007febc68e3140 @value=8, @left=#<Node:0x00007febc68e1e80 @value=1, @left=nil, @right=#<Node:0x00007febc68eff58 @value=3, @left=nil, @right=#<Node:0x00007febc68ed1e0 @value=4, @left=nil, @right=#<Node:0x00007febc6888ee8 @value=5, @left=nil, @right=#<Node:0x00007febc68681e8 @value=7, @left=nil, @right=nil>>>>>, @right=#<Node:0x00007febc683b6e8 @value=9, @left=nil, @right=#<Node:0x00007febc68314b8 @value=23, @left=nil, @right=#<Node:0x00007febc681a3f8 @value=67, @left=nil, @right=#<Node:0x00007febc6812ea0 @value=324, @left=nil, @right=#<Node:0x00007febc6809238 @value=6345, @left=nil, @right=nil>>>>>>
-  end
-  
-  def insert(value)
-    # should always be added as a leaf
   end
   
   def delete(value)
@@ -128,7 +126,7 @@ class Tree
 end
 
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-tree = Tree.new(array) # => #<Tree:0x00007febc80a8168 @array=[1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324], @root=#<Node:0x00007febc68e3140 @value=8, @left=#<Node:0x00007febc68e1e80 @value=1, @left=nil, @right=#<Node:0x00007febc68eff58 @value=3, @left=nil, @right=#<Node:0x00007febc68ed1e0 @value=4, @left=nil, @right=#<Node:0x00007febc6888ee8 @value=5, @left=nil, @right=#<Node:0x00007febc68681e8 @value=7, @left=nil, @right=nil>>>>>, @right=#<Node:0x00007febc683b6e8 @value=9, @left=nil, @right=#<Node:0x00007febc68314b8 @value=23, @left=nil, @right=#<Node:0x00007febc681a3f8 @value=67, @left=nil, @right=#<Node:0x00007febc6812ea0 @value=324, @left=nil, @right=#<Node:0x00007febc6809238 @value=6345, @left=nil, @right=nil>>>>>>>
+tree = Tree.new(array) # => #<Tree:0x00007faace89e600 @array=[1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324], @root=#<Node:0x00007faace89e178 @value=8, @left=#<Node:0x00007faace89dd40 @value=1, @left=nil, @right=#<Node:0x00007faace89d958 @value=3, @left=nil, @right=#<Node:0x00007faace89d228 @value=4, @left=nil, @right=#<Node:0x00007faace89c6e8 @value=5, @left=nil, @right=#<Node:0x00007faacf057158 @value=7, @left=nil, @right=nil>>>>>, @right=#<Node:0x00007faace8a3970 @value=9, @left=nil, @right=#<Node:0x00007faace8a35d8 @value=23, @left=nil, @right=#<Node:0x00007faace8a2ed0 @value=67, @left=nil, @right=#<Node:0x00007faace8a23b8 @value=324, @left=nil, @right=#<Node:0x00007faace8a13f0 @value=6345, @left=nil, @right=nil>>>>>>>
 
 # Tree building tests
 tree.root.left.value == 1 # => true
@@ -144,3 +142,6 @@ tree.root.right.right.right.value == 67 # => true
 tree.root.right.right.right.right.value == 324 # => true
 tree.root.right.right.right.right.right.value == 6345 # => true
 tree.root.right.right.right.right.right.right == nil # => true
+
+# Insert test
+tree.root.left.right.right.left.value == 6 # => true
